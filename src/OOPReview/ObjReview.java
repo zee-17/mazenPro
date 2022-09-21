c94fbbb699814e530addf6ab79a8da76df28a77d
Mazen Kaher <mazenkasher17@gmail.com>
2022-09-21 16:39:57 -0400

    Creating obj class

---
+++ src/OOPReview/ObjReview.java
@@ -0,0 +1,8 @@
+package OOPReview;
+
+public class ObjReview {
+    public static void main(String[] args) {
+//        Male erwa = new Male(); // Add variables here
+//        Female elena = new Female(); // Add the variables here
+    }
+}
